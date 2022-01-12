251cf84c0961b6ed30909daa270b890c71d7d8ea
Joshua Boyd <jaboydfamily@gmail.com>
2022-01-11 21:52:03 -0500

    got square grid working

--- etch.js
+++ etch.js
@@ -1,12 +1,19 @@
-function createGrid() {
+function createGrid(squareNum) {
 	// retrieve grid container
 	let etchContainer = document.querySelector("#gridContainer");
 
-	for(let i=0; i<16; i++) {
-		let gridSquare = document.createElement('div');
-		gridSquare.classList.add('gridSquare');
-		etchContainer.appendChild(gridSquare);
+	// row
+	for(let i=1; i<=squareNum; i++) {
+		// column
+		for(let a=1; a<=squareNum; a++) {
+			let gridSquare = document.createElement('div');
+			gridSquare.setAttribute('style', `grid-column: ${a};
+											  grid-row: ${i};
+											  background-color: red;
+											  border: 1px solid black;`);
+			etchContainer.appendChild(gridSquare);
+		}
 	}
 }
 
-createGrid();
+createGrid(50);
